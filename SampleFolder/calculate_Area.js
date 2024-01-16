44ce9a9dbda6313aac9b4df4dd0441c7d8d1e1c7
Carlos Lucena <carlos_lucena@hotmail.com>
2024-01-15 20:59:32 -0500

    Functions

--- SampleFolder/calculate_Area.js
+++ SampleFolder/calculate_Area.js
@@ -4,7 +4,19 @@ let width;
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
-    let are = length * width
+    let area = length * width
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker(amounts) {
+    amounts = [parseFloat(document.getElementById('amount1').value),
+    parseFloat(document.getElementById('amount2').value),
+    parseFloat(document.getElementById('amount3').value)]
+    addAmountAndUpdateTotal(amounts)
+} 
+
+function addAmountAndUpdateTotal(amounts) {
+    let total = amounts.reduce((acc, value) => acc + value)
+    document.getElementById('Total').innerText = `The total amount is: ${total}`
+} 
