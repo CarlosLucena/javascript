f14add40cf0730a33db9bf60aec26b9cab24910c
Carlos Lucena <carlos_lucena@hotmail.com>
2024-03-27 01:52:37 -0400

    Refactored menu item build

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,23 +1,13 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes - $12.00', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
-const populateFunc = (item, index) => `<p>Item ${index + 1}: ${item}</p>`
-const breakfastMenuItemsHTML = breakfastMenu
-  .map(populateFunc)
-  .join('');
-document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+const joinFunc = (acc, item) => acc + item
+const buildItemFunc = (item, index) => {return `<p>Item ${index + 1}: ${item}</p>`}
+const listItemFunc = (arr) => arr
+  .map(buildItemFunc)
+  .reduce(joinFunc, '')
 
-let mainCourseItem = '';
-const populateMainMenuFunc = (item, index) => {
-    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;}
-
-mainCourseMenu.forEach(populateMainMenuFunc);
-document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
-
-let dessertItem = '';
-//const populateDessertFunc = 
-for (let i = 0; i < dessertMenu.length; i++) {
-    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
-document.getElementById('dessertMenuItems').innerHTML = dessertItem;
-
+document.getElementById('breakfastMenuItems').innerHTML = listItemFunc(breakfastMenu);
+document.getElementById('maincourseMenuItems').innerHTML = listItemFunc(mainCourseMenu);
+document.getElementById('dessertMenuItems').innerHTML = listItemFunc(dessertMenu);
