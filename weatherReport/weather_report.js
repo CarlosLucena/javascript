0a7218a481c5af9a952cf357e5281f5fe057496c
CarlosLucena <carlos_lucena@hotmail.com>
2024-11-25 01:53:32 -0500

    Extra api call.

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -19,5 +19,29 @@ function showweatherDetails(event) {
         weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
       });
 }
+document.getElementById('getWeatherButton').addEventListener('click',showweatherDetails );
+//document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
 
-document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+function showweatherDetailsByLatLon(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('lat').value
+    const lon = document.getElementById('lon').value
+    const apiKey = 'b63b325384c3258104d5c571cfe3acfe' 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                 <p>Temperature: ${data.main.temp} &#8451;</p>
+                                 <p>Weather: ${data.weather[0].description}</p>`;
+    }).catch(error => {
+      console.error('Error fetching weather:', error);
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
+document.getElementById('getWeatherByLatLonButton').addEventListener('click',showweatherDetailsByLatLon );
+//document.getElementById('weatherForm').addEventListener('submit',showweatherDetailsByLatLon );
